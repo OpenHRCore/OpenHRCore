69df40a4587a157f7788d69df291736c8062da5c
Stephen Kyaw <kyawmyoaung.dev@gmail.com>
2024-09-08 02:11:03 +0700

    add DependencyInjection for OpenHRCoreWorkForceDbContext and DBCS

---
+++ src/WorkForce/OpenHRCore.WorkForce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OpenHRCore.WorkForce.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddOpenHRCoreWorkForceDbContext(this IServiceCollection services, IConfiguration _configuration)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<OpenHRCoreWorkForceDbContext>(options => options.UseSqlServer(connectionString));
+            return services;
+        }
+    }
+}
