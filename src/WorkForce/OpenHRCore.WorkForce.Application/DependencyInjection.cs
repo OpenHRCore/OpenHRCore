133a57ef5a6048755f1a48ee85ee0bfd44862ce6
Stephen Kyaw <kyawmyoaung.dev@gmail.com>
2024-09-09 00:44:59 +0700

    FluentValidation: add auto validation

--- src/WorkForce/OpenHRCore.WorkForce.Application/DependencyInjection.cs
+++ src/WorkForce/OpenHRCore.WorkForce.Application/DependencyInjection.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using OpenHRCore.WorkForce.Application.Interfaces;
 using OpenHRCore.WorkForce.Application.Mapping;
 using OpenHRCore.WorkForce.Application.Services;
+using System.Reflection;
 
 namespace OpenHRCore.WorkForce.Application
 {
@@ -10,6 +12,8 @@ namespace OpenHRCore.WorkForce.Application
         public static IServiceCollection AddOpenHRCoreWorkForceApplication(this IServiceCollection services)
         {
             services.AddAutoMapper(typeof(WorkForceMappingProfile));
+            //services.AddValidatorsFromAssemblyContaining<CreateJobGradeRequestValidator>();
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddScoped<IJobPositionService, JobPositionService>();
             return services;
         }
